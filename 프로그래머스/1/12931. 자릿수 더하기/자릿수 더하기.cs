f89f6b35b3db3302a80153c6d13148568d8922d5
제갈도원 <144315718+Jegaldowon@users.noreply.github.com>
2026-01-04 02:45:42 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.09 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        
+        string s = n.ToString();
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            answer += int.Parse(s[i].ToString());
+        }
+        
+        
+        return answer;
+    }
+}
