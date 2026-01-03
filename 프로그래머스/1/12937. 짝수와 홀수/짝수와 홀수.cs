c062916100dde32b76ba465588cf2a7f52d30c7b
제갈도원 <144315718+Jegaldowon@users.noreply.github.com>
2026-01-04 03:16:13 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.15 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -0,0 +1,21 @@
+
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = "";
+
+            if (num % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+
+
+
+            return answer;
+        }
+    }
