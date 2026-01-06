0f07129b118a79aed2f90f78e6cf808789b78e16
제갈도원 <144315718+Jegaldowon@users.noreply.github.com>
2026-01-06 16:27:16 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
@@ -7,14 +7,22 @@ public class Solution
         // num = 3;
         // total = 12;
         // result = 3,4,5;
-
+        // total / 3 = 4 -> 가운데 값
+        
+        // i = 0 -> 3;
+        // i = 1 -> 4;
+        // i = 2 -> 5;
+        
+        // num = 4;
+        // total = 14;
+        // res = 2,3,4,5
+        // 가운데 값 : 3 
+        
         if (num % 2 == 0)
         {
             for (global::System.Int32 i = 0; i < num; i++)
             {
-                // i = 0 -> 3;
-                // i = 1 -> 4;
-                // i = 2 -> 5;
+               
                 answer[i] = (total / num) - (num / 2) + i + 1;
             }
         }
