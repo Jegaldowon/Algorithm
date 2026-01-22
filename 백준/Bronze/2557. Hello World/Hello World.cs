63c23ca67d31790a042fc1c26c8570810c3f5939
제갈도원 <144315718+Jegaldowon@users.noreply.github.com>
2026-01-23 03:49:39 +0900

    [Bronze V] Title: Hello World, Time: 44 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+   
+   
+   
+}
