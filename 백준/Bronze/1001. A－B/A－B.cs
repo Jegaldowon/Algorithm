9fd01378ebe476c623e61d623e4a91ed7119d7f0
제갈도원 <144315718+Jegaldowon@users.noreply.github.com>
2026-01-23 03:49:57 +0900

    [Bronze V] Title: A-B, Time: 52 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input= Console.ReadLine().Split();
+
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+
+        int sum = a - b;
+        Console.WriteLine(sum);
+    }
+   
+   
+   
+}
